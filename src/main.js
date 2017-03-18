be2088f61a520d5c979d890b67587c5e47b8c748
Ben Kremer <ben.kremer@hotmail.co.uk>
2017-03-18 15:02:13 +0100

    Linting

--- src/main.js
+++ src/main.js
@@ -3,27 +3,27 @@
  */
 
 // Polyfill
-import 'babel-polyfill';
+import "babel-polyfill"
 
 // Libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, browserHistory } from 'react-router';
+import React from "react"
+import ReactDOM from "react-dom"
+import { Router, browserHistory } from "react-router"
 
 // Routes
-import Routes from './common/components/Routes';
+import Routes from "./common/components/Routes"
 
 // Base styling
-import './common/base.css';
+import "./common/base.css"
 
 
 // ID of the DOM element to mount app on
-const DOM_APP_EL_ID = 'app';
+const DOM_APP_EL_ID = "app"
 
 // Render the router
 ReactDOM.render((
   <Router history={browserHistory}>
     {Routes}
   </Router>
-), document.getElementById(DOM_APP_EL_ID));
+), document.getElementById(DOM_APP_EL_ID))
 
