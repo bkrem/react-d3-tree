5be9515e127cf4829fa49b1fbd0df8fb31d2a6df
Ben Kremer <ben.kremer@hotmail.co.uk>
2017-05-20 17:32:05 +0200

    Adds zoom capability and unit test, adds JSDocs for Tree (#4)

--- src/Tree/tests/index.test.js
+++ src/Tree/tests/index.test.js
@@ -155,4 +155,23 @@ describe('<Tree />', () => {
 
     expect(Tree.prototype.setInitialTreeDepth).toHaveBeenCalled();
   });
+
+  it('allows zooming in/out according to `props.scaleExtent` if `props.zoomable`', () => {
+    const zoomableComponent = mount(
+      <Tree
+        data={mockData}
+      />
+    );
+    const nonZoomableComponent = mount(
+      <Tree
+        data={mockData}
+        zoomable={false}
+      />
+    );
+
+    zoomableComponent.find('svg').simulate('touchmove');
+
+    expect(zoomableComponent.find('svg').prop('transform')).toBeDefined();
+    expect(nonZoomableComponent.find('svg').prop('transform')).toBeUndefined();
+  });
 });
