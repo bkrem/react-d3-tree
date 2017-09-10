9c054d9b4a867286e223d2e3430fa239b4a903c1
Ben Kremer <ben.kremer@hotmail.co.uk>
2017-09-10 16:22:33 +0200

    Disable parameter assignment lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,7 +56,7 @@ module.exports = {
     "newline-per-chained-call": 0,
     "no-confusing-arrow": 0,
     "no-console": 1,
-    "no-param-reassign": "warn",
+    "no-param-reassign": "off",
     "no-use-before-define": 0,
     "no-underscore-dangle": "off",
     "no-unused-expressions": "warn",
