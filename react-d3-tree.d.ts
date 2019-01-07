4c2feb45ca3f9c718bcdc05e8c29229ed5aa9a51
Ben Kremer <ben.kremer@hotmail.co.uk>
2019-01-07 16:59:24 +0100

    Declares public types in react-d3-tree.d.ts

---
+++ react-d3-tree.d.ts
@@ -0,0 +1,64 @@
+declare module "react-d3-tree" {
+  type NodeSvgShape = {
+    shape?: string,
+    shapeProps?: object
+  }
+
+  type ReactD3TreeItem = {
+    name: string,
+    attributes?: {
+      [key: string]: string,
+    };
+    children?: ReactD3TreeItem[],
+    _collapsed?: boolean,
+    nodeSvgShape?: NodeSvgShape
+  };
+
+  type ReactD3TreeProps = {
+    data: ReactD3TreeItem[] | ReactD3TreeItem,
+    nodeSvgShape?: NodeSvgShape,
+    nodeLabelComponent?: object,
+    onClick?: (targetNode: ReactD3TreeItem, event: Event) => any,
+    onMouseOver?: (targetNode: ReactD3TreeItem, event: Event) => any,
+    onMouseOut?: (targetNode: ReactD3TreeItem, event: Event) => any,
+    onUpdate?: (targetNode: ReactD3TreeItem, event: Event) => any,
+    orientation?: "horizontal" | "vertical",
+    translate?: {
+      x?: number,
+      y?: number
+    },
+    pathFunc?: ("diagonal" | "elbow" | "straight") | ((...args: any[]) => any),
+    transitionDuration?: number,
+    depthFactor?: number,
+    collapsible?: boolean,
+    useCollapseData?: boolean,
+    initialDepth?: number,
+    zoomable?: boolean,
+    zoom?: number,
+    scaleExtent?: {
+      min?: number,
+      max?: number
+    },
+    nodeSize?: {
+      x?: number,
+      y?: number
+    },
+    separation?: {
+      siblings?: number,
+      nonSiblings?: number
+    },
+    textLayout?: object,
+    allowForeignObjects?: boolean,
+    shouldCollapseNeighborNodes?: boolean,
+    circleRadius?: number,
+    styles?: {
+      nodes?: object,
+      links?: object
+    }
+  };
+
+  var Tree: React.ComponentClass<ReactD3TreeProps, any>;
+  
+  export {Tree};
+  export default Tree;
+}
