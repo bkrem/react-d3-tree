6af5f485de022ef54ef94c099af2466103ad40c5
Ben Kremer <ben.kremer@hotmail.co.uk>
2020-01-15 19:10:22 +0100

    chore(env): sets up Jest for parallel TS + JS

--- jest/setup.ts
+++ jest/setup.ts
@@ -1,6 +1,6 @@
-/* eslint-disable */
 import raf from './polyfills/raf'
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+// @ts-ignore
 configure({ adapter: new Adapter() });
