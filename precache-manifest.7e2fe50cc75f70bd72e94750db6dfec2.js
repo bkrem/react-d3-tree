d17b1c05bcd68468067d89623dddabccb28d61e6
Ben Kremer <ben.kremer@hotmail.co.uk>
2020-12-20 15:39:27 +0100

    Updates

---
+++ precache-manifest.7e2fe50cc75f70bd72e94750db6dfec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3d09d2939233d4d6f5d29e5d5b0ddb",
+    "url": "/react-d3-tree/index.html"
+  },
+  {
+    "revision": "2544c9ade5b89e9630ac",
+    "url": "/react-d3-tree/static/css/main.089afecc.chunk.css"
+  },
+  {
+    "revision": "1ae8dfe3110522c28217",
+    "url": "/react-d3-tree/static/js/2.5fd6b926.chunk.js"
+  },
+  {
+    "revision": "4267de99f6d639cc5d1750b65c435190",
+    "url": "/react-d3-tree/static/js/2.5fd6b926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2544c9ade5b89e9630ac",
+    "url": "/react-d3-tree/static/js/main.2d8e92a7.chunk.js"
+  },
+  {
+    "revision": "9111a4c6b626f402f6fa",
+    "url": "/react-d3-tree/static/js/runtime-main.bcddc8b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-d3-tree/precache-manifest.e494fd3b8e635d255a5c1b5f35ba9eda.js"
+  "/react-d3-tree/precache-manifest.7e2fe50cc75f70bd72e94750db6dfec2.js"
 );
 
 self.addEventListener('message', (event) => {
