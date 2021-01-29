1e6e97826414c36fec3be74f99ec1b51ac33e15d
Ben Kremer <ben.kremer@hotmail.co.uk>
2021-01-29 17:16:59 +0100

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,85 +1,15 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": [
-    "airbnb",
-    "prettier"
+  parser: 'babel-eslint',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+    },
   ],
-  "env": {
-    "browser": true,
-    "node": true,
-    "jest": true,
-    "es6": true
-  },
-  "plugins": [
-    "react",
-    "jsx-a11y"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true
-    }
-  },
-  "rules": {
-    "arrow-parens": [
-      "error",
-      "as-needed"
-    ],
-    "arrow-body-style": [
-      2,
-      "as-needed"
-    ],
-    "comma-dangle": [
-      2,
-      "always-multiline"
-    ],
-    "import/imports-first": 0,
-    "import/newline-after-import": 0,
-    "import/no-dynamic-require": 0,
-    "import/no-extraneous-dependencies": 0,
-    "import/no-named-as-default": 0,
-    "import/no-unresolved": 2,
-    "import/prefer-default-export": 0,
-    // "indent": [
-    //   2,
-    //   2,
-    //   {
-    //     "SwitchCase": 1
-    //   }
-    // ],
-    "jsx-a11y/aria-props": 2,
-    "jsx-a11y/heading-has-content": 0,
-    "jsx-a11y/href-no-hash": 0,
-    "jsx-a11y/img-has-alt": 0,
-    "jsx-a11y/anchor-is-valid": 0,
-    "jsx-a11y/label-has-for": 2,
-    "jsx-a11y/mouse-events-have-key-events": 0,
-    "jsx-a11y/role-has-required-aria-props": 2,
-    "jsx-a11y/role-supports-aria-props": 2,
-    "max-len": 0,
-    "newline-per-chained-call": 0,
-    "no-confusing-arrow": 0,
-    "no-console": 1,
-    "no-param-reassign": 0,
-    "no-use-before-define": 0,
-    "no-underscore-dangle": 0,
-    "no-unused-expressions": 0,
-    "no-unused-vars": 1,
-    "prefer-template": 2,
-    "class-methods-use-this": 0,
-    "react/forbid-prop-types": 0,
-    "react/jsx-first-prop-new-line": [
-      2,
-      "multiline"
-    ],
-    "react/jsx-filename-extension": 0,
-    "react/jsx-no-target-blank": 0,
-    "react/jsx-indent": 0,
-    "react/require-extension": 0,
-    "react/no-unused-prop-types": 1,
-    "react/self-closing-comp": 0,
-    "react/sort-comp": 0,
-    "require-jsdoc": "warn"
-  },
 };
