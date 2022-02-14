e84fab364beb4f531d6a92a0f406ae6138af2066
Yohann Fabri <11197007+YoranSys@users.noreply.github.com>
2022-02-14 14:12:13 +0100

    chore(css): improvement of the readability
    - Label and attributes use fill instead of stoke
    - Visual differentiation of leaf and node

--- src/globalCss.ts
+++ src/globalCss.ts
@@ -36,17 +36,17 @@ export default `
   cursor: pointer;
   fill: transparent;
   stroke: #000;
-  stroke-width: 2;
+  stroke-width: 1;
 }
 
 .rd3t-label__title {
-  stroke: #000;
-  stroke-width: 1;
+  fill: #000;
+  font-weight: bolder;
 }
 
 .rd3t-label__attributes {
-  stroke: #777;
-  stroke-width: 1;
+  fill: #777;
+  font-weight: bolder;
   font-size: smaller;
 }
 
