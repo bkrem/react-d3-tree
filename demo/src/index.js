fdaaacd47c5fe807becfbe7ddf8c70a8da76dfe1
Ben Kremer <contact@bkrem.dev>
2022-07-12 23:45:15 +0700

    chore(demo): update to react@18 and react-d3-tree@3.3.4

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+createRoot(document.getElementById('root')).render(<App />);
