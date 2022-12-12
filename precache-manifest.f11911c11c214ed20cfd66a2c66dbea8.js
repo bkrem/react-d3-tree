625befec2c9090b16639e5a4b527a9b350fc22d3
Ben Kremer <contact@bkrem.dev>
2022-12-12 20:51:16 +0100

    Updates

--- precache-manifest.f11911c11c214ed20cfd66a2c66dbea8.js
+++ precache-manifest.f11911c11c214ed20cfd66a2c66dbea8.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b5f67fc3d5d039425b808e32117c31d0",
+    "revision": "9127dffac32a7602e762305addbb0a34",
     "url": "/react-d3-tree/index.html"
   },
   {
-    "revision": "c207775e926764e00d75",
+    "revision": "45edc4498924da543b0f",
     "url": "/react-d3-tree/static/css/main.26a6a201.chunk.css"
   },
   {
-    "revision": "ed418e44d461a1f4d782",
-    "url": "/react-d3-tree/static/js/2.dfbba941.chunk.js"
+    "revision": "45f328c93edcc130aa05",
+    "url": "/react-d3-tree/static/js/2.810397c3.chunk.js"
   },
   {
     "revision": "e26de4f0e0d98f1626b38316e0457409",
-    "url": "/react-d3-tree/static/js/2.dfbba941.chunk.js.LICENSE.txt"
+    "url": "/react-d3-tree/static/js/2.810397c3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c207775e926764e00d75",
-    "url": "/react-d3-tree/static/js/main.8d48ab2e.chunk.js"
+    "revision": "45edc4498924da543b0f",
+    "url": "/react-d3-tree/static/js/main.c97a38cd.chunk.js"
   },
   {
     "revision": "9111a4c6b626f402f6fa",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-d3-tree/precache-manifest.1c91a583e7b6cec2a7e92df3bf0e0520.js"
+  "/react-d3-tree/precache-manifest.f11911c11c214ed20cfd66a2c66dbea8.js"
 );
 
 self.addEventListener('message', (event) => {
